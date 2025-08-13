5e8548fff074fc70d65f2574563047a841ab0644
Your Name <you@example.com>
2025-08-13 13:38:10 +0530

    first commit

--- auto_fetch_pull_push.sh
+++ auto_fetch_pull_push.sh
@@ -3,9 +3,14 @@
 
 
 git fetch
+echo ">>> Success: FETCH"
 git pull
+echo ">>> Success: PULL"
 
 git add *
+echo ">>> Success: ADD"
 read message
-git commit -m $message
+git commit -m "$message"
+echo ">>> Success: COMMIT"
 git push
+echo ">>> Success: PUSH"
